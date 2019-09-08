964189443d59948dac1ce10bbef55b16f760832c
Lunev Artem <artem.e.lunev2000@gmail.com>
2019-09-08 05:09:55 +0300

    task01

--- example/main.sh
+++ example/main.sh
@@ -1,2 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
+max_value=0
+studet=0
+values=()
+
+while read students
+do
+	((values[students]+=1))
+done
+for i in ${!values[@]}
+do
+        if [ $max_value -lt ${values[i]} ]
+	then
+		max_value=${values[i]}
+		student=$i
+	fi
+done
+
+echo $student
